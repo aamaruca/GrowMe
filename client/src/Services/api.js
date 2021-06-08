f83f425ed256978b0f8e5d91e97f87fced132b42
Angela Maruca <angelamaruca@Angelas-MacBook-Pro.local>
2021-06-08 11:20:06 -0400

    protected api key

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -3,18 +3,19 @@
 //create and store all axios requests here
 import axios from "axios"
 
-// const apiKey = process.env.REACT_APP_AIRTABLE_KEY
-const baseUrl = "https://api.airtable.com/v0/appy8EATGezR6VvHz/Display%20and%20Create?api_key=keyWdRg21om9S456K"
+const apiKey = process.env.REACT_APP_AIRTABLE_KEY
+const baseUrl = "https://api.airtable.com/v0/appy8EATGezR6VvHz/Display%20and%20Create"
 
-// const config = {
-//   headers: {
-//     Authorization: `Bearer ${apiKey}`,
-//   },
-// }
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+  },
+}
 
 export const getPlantList = async () => {
   try {
-    const res = await axios.get(baseUrl)
+    const res = await axios.get(baseUrl, config)
     return res.data.records
   } catch (error) {
     console.error(error)
