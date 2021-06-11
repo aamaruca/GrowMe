f114fbd98448322c1aba1fdf04074c6fdca0be21
Angela Maruca <angelamaruca@Angelas-MacBook-Pro.local>
2021-06-10 23:37:07 -0400

    styled add plant component

--- client/src/Components/AddPlant/AddPlant.jsx
+++ client/src/Components/AddPlant/AddPlant.jsx
@@ -1,59 +1,54 @@
-//import hooks and axios post request from api,js
-//import navbar and links via buttons
-//import css
-//create form for post request
-//create handlesubmit and handlechange events
-//pmvp create toggle to reroute back to plantlist on submit instead of navbar
-import {useState} from 'react'
-import {useHistory} from 'react-router-dom'
-import NavBar2 from '../NavBar/NavBar2'
-import './AddPlant.css'
-import {postNewPlant} from '../../Services/api'
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import NavBar2 from "../NavBar/NavBar2";
+import "./AddPlant.css";
+import { postNewPlant } from "../../Services/api";
 
 export default function AddPlant() {
-  const [formData, setFormData] = useState({})
-  const history = useHistory()
+  const [formData, setFormData] = useState({});
+  const history = useHistory();
 
   const handleChange = (e) => {
-    const { name, value } = e.target
-    
+    const { name, value } = e.target;
+
     setFormData({
       ...formData,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await postNewPlant(formData);
     console.log(res);
-    history.push("/plants")
+    history.push("/plants");
   };
 
   return (
-    <div>
+    <>
       <NavBar2 />
-      <h2>Plant Notes</h2>
-      <form onChange={handleChange} onSubmit={handleSubmit}>
-        <label>Plant Name: </label>
-        <input type="text" name="name"></input><br/>
-        <br/><label>Date Planted: </label>
-        <input type="date" name="datePlanted"></input><br />
-        <br/><label>Location: </label>
-        <input type="text" name="location"></input><br />
-        <br/><label>Purchased from: </label>
-        <input type="text" name="purchased"></input><br />
-        <br/><label>Sun Exposure: </label>
-        <input type="text" name="sunExposure"></input><br />
-        <br/><label>Bloom Color: </label>
-        <input type="text" name="bloomColor"></input><br />
-        <br/><label>Bloom Season: </label>
-        <input type="text" name="bloomSeason"></input><br />
-        <br/><label>General Notes: </label>
-        <br/><textarea  name="notes"
-          rows={10} /><br />
-      <button>Plant Me!</button>
-      </form>
-  
-    </div>
-  )
+      <div className="data">
+        <h1>Plant Notes</h1>
+        <form className="form" onChange={handleChange} onSubmit={handleSubmit}>
+          <input className="input" type="text" name="name" placeholder="Plant Name"></input>
+          <br />
+          <input className="input" type="date" name="datePlanted" placeholder="Date Planted"></input>
+          <br />
+          <input className="input" type="text" name="location" placeholder="Location Planted"></input>
+          <br />
+          <input className="input" type="text" name="purchased" placeholder="Purchased from"></input>
+          <br />
+          <input className="input" type="text" name="sunExposure" placeholder="Sun Exposure"></input>
+          <br />
+          <input className="input" type="text" name="bloomColor" placeholder="Bloom Color"></input>
+          <br />
+          <input className="input" type="text" name="bloomSeason" placeholder="Month of Bloom"></input>
+          <br />
+          <label className= "notes">General Notes: </label>
+          <textarea className="textarea" name="notes" rows={10} placeholder="Enter notes here..." />
+          <br />
+          <button className="addbtn">Plant Me!</button>
+        </form>
+      </div>
+    </>
+  );
 }
